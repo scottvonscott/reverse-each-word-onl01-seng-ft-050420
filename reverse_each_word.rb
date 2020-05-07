3549d61075b930bf5a788b56aeab4646a7301d5c
Scott  Keplinger <skeplin17@gmail.com>
2020-05-07 20:05:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (sentence)
   new_array = sentence.split
   reversed_array = []
-  new_array.map do |words|
+  new_array.collect do |words|
     reversed_array << words.reverse
   end
   reversed_array.join(" ")
